a0188af5beebff23d2bc29bc7f614abf9c9aa009
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-08-08 22:29:03 +0200

    FEAT: create Ticket.routes.js

---
+++ routes/Ticket.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+const controller = require('../controllers/ticket.controller');
+
+
+router.get('/', controller.getTickets);
+
+// router.post('/create', controller.createTickets);
+
+// router.put('/edit-ticket', controller.ediTickets);
+
+// router.delete('/delete-ticket', controller.deleteTickets);
+
+
+
+module.exports = router;
