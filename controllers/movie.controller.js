c4566b8777919113637bd5150da1e9969c3d07a2
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-08-08 02:09:18 +0200

    FEAT: add createMovies controler

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -5,14 +5,32 @@ const getMovies = async(req, res, next) => {
         const movies = await Movies.find();
         return res.status(200).json(movies)
     } catch (error) {
-        next(error);
+        return next(error);
     }
 };
 
 
-const moviePost = (req, res, next) => {
+const createMovies = async(req, res, next) => {
+    try {
+        const { title, year, director, actors, country, duration, summary, filmProducer, gender } = req.body;
+        const newMovie = new Movies({
+            title,
+            year,
+            director,
+            actors,
+            country,
+            duration,
+            summary,
+            filmProducer,
+            gender
+        });
 
+        const createdMovie = await newMovie.save();
+        return res.status(200).json(createdMovie);
+    } catch (error) {
+        return next(error);
+    }
 }
 
 
-module.exports = { getMovies, moviePost };
+module.exports = { getMovies, createMovies };
